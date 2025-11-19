f6e4fc5aa89f1f930f6f278af206d1cc5c6b7f71
thanu.venu28@gmail.com <thanu.venu28@gmail.com>
2025-11-19 09:18:09 +0530

    added extra feature to change background automatically every 5 seconds

--- Day-01-Random-Background/script.js
+++ Day-01-Random-Background/script.js
@@ -1,6 +1,11 @@
 const button=document.getElementById('change-bg-button');
 
-button.addEventListener('click',function(){
+function changeBackground(){
     const randomColor='#' + Math.floor(Math.random()*16777215).toString(16);
     document.body.style.backgroundColor=randomColor;
-})
+}
+// Change background when button is clicked
+button.addEventListener('click',changeBackground);
+
+// Bonus: Change background automatically every 5 seconds
+setInterval(changeBackground,5000);
