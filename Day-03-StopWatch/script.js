c7d58cf857b13b3849ee9b271a1cded7fb9fb75a
thanu.venu28@gmail.com <thanu.venu28@gmail.com>
2025-11-21 11:30:47 +0530

    added extra features like lap,dark theme,circular animation,local storage

--- Day-03-StopWatch/script.js
+++ Day-03-StopWatch/script.js
@@ -3,13 +3,24 @@ let seconds=0;
 let minutes=0;
 let timer=null;
 let running=false;
+let progress=0;
+
+const display = document.getElementById("display");
+const lapsList = document.getElementById("laps");
+
+window.onload = () => {
+    loadLaps();
+};
 
 function updateDisplay(){
     let m=minutes<10 ? "0"+ minutes : minutes;
     let s=seconds<10 ? "0"+ seconds : seconds;
     let ms=milliseconds<10 ? "0"+ milliseconds : milliseconds;
 
-    document.getElementById('display').innerText=`${m}:${s}:${ms}`;
+    display.innerText=`${m}:${s}:${ms}`;
+
+    document.querySelector(".progress-ring").style.transform=`rotate(${progress}deg)`;
+    progress+=3.6;
 
 }
 
@@ -43,11 +54,58 @@ function reset(){
     seconds=0;
     minutes=0;
     updateDisplay();
+     // Clear lap history in the UI
+    lapsList.innerHTML = '';
 
+    // Clear lap history in localStorage
+    localStorage.removeItem('laps');
+
+}
+
+function lap(){
+    if(!running) return;
+
+    let lapTime=display.innerText;
+
+    let li=document.createElement("li");
+    li.innerText=lapTime;
+
+    lapsList.appendChild(li);
+
+    saveLap(lapTime);
+}
+
+function saveLap(time){
+    let laps=JSON.parse(localStorage.getItem("laps")) || [];
+    laps.push(time);
+    localStorage.setItem("laps",JSON.stringify(laps));
+}
+
+function loadLaps(){
+    let laps=JSON.parse(localStorage.getItem("laps")) || [];
+
+    laps.array.forEach(t => {
+        let li=document.createElement("li");
+        li.innerText=t;
+        lapsList.appendChild(li);
+        
+    });
+}
+
+document.getElementById("mode").onclick = () => {
+    document.body.classList.toggle("dark");
 }
 
 document.getElementById("start").addEventListener("click",start);
 document.getElementById('pause').addEventListener('click',pause);
 document.getElementById('reset').addEventListener('click',reset);
+document.getElementById("lap").onclick = lap;
+
+document.addEventListener("keydown",(e)=>{
+    if (e.key === " ") start();        // Space = Start
+    if (e.key === "p") pause();        // P = Pause
+    if (e.key === "r") reset();        // R = Reset
+    if (e.key === "l") lap();          // L = Lap
+});
 
 updateDisplay();
