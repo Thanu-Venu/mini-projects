aadf274ebde21d3132f4acbeba53a57e36827b3d
thanu.venu28@gmail.com <thanu.venu28@gmail.com>
2025-11-28 08:57:01 +0530

    added basic features

---
+++ Day-10-Image-Carousel/script.js
@@ -0,0 +1,33 @@
+let index = 0;
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+
+function showSlide(i) {
+    slides.forEach(slide => slide.classList.remove("active"));
+    dots.forEach(dot => dot.classList.remove("active-dot"));
+
+    slides[i].classList.add("active");
+    dots[i].classList.add("active-dot");
+}
+
+function nextSlide() {
+    index = (index + 1) % slides.length;
+    showSlide(index);
+}
+
+function prevSlide() {
+    index = (index - 1 + slides.length) % slides.length;
+    showSlide(index);
+}
+
+document.querySelector(".next").addEventListener("click", nextSlide);
+document.querySelector(".prev").addEventListener("click", prevSlide);
+
+dots.forEach((dot, i) => {
+    dot.addEventListener("click", () => {
+        index = i;
+        showSlide(index);
+    });
+});
+
+setInterval(nextSlide, 3000);
